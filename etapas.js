5816ffc843e9056d329cb0150b77c8819a879b08
unknown <clara.livia.moura62@aluno.ifce.edu.br>
2021-12-05 11:06:14 -0300

    update 05-12-2021

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etappas =[
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos:[
+            {
+                numero: 38111,
+                name: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos:[
+                    {url:'AA.png', legenda:'Vereador', small: false}
+                ]
+            },
+            {
+                numero: 77222,
+                name: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos:[
+                    {url:'XX.png', legenda:'Vereador', small: false}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos:[
+            {
+                numero: 99,
+                name: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos:[
+                    {url:'BB-1.png', legenda:'Prefeito', small: true},
+                    {url:'BB-2.png', legenda:'Vice-prefeito', small: true}
+                ]
+            },
+            {
+                numero: 84,
+                name: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos:[
+                    {url:'ZZ-1.png', legenda:'Prefeito', small: true},
+                    {url:'ZZ-2.png', legenda:'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    },
+]
